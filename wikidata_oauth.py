25d9a49f19713db63809fa9c55802ff65f463b2c
Ederporto <eder.porto@hotmail.com>
2020-04-11 23:28:32 -0300

    Adding login as implemented in WADE tool - wikidata_oauth

---
+++ wikidata_oauth.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#
+# This file is a copy of the file with the same name on the WADE projetc,
+# available here: https://github.com/EdwardBetts/depicts in the folder
+# "depicts" under the GPL-3 license. Some modifications were made due
+# to variable names
+
+from flask import current_app, session
+from requests_oauthlib import OAuth1Session
+from urllib.parse import urlencode
+
+
+def api_post_request(params):
+    app = current_app
+    url = 'https://www.wikidata.org/w/api.php'
+    client_key = app.config['CONSUMER_KEY']
+    client_secret = app.config['CONSUMER_SECRET']
+    oauth = OAuth1Session(client_key,
+                          client_secret=client_secret,
+                          resource_owner_key=session['owner_key'],
+                          resource_owner_secret=session['owner_secret'])
+    return oauth.post(url, data=params, timeout=4)
+
+
+def raw_request(params):
+    app = current_app
+    url = 'https://www.wikidata.org/w/api.php?' + urlencode(params)
+    client_key = app.config['CONSUMER_KEY']
+    client_secret = app.config['CONSUMER_SECRET']
+    oauth = OAuth1Session(client_key,
+                          client_secret=client_secret,
+                          resource_owner_key=session['owner_key'],
+                          resource_owner_secret=session['owner_secret'])
+    return oauth.get(url, timeout=4)
+
+
+def api_request(params):
+    return raw_request(params).json()
+
+
+def get_token():
+    params = {
+        'action': 'query',
+        'meta': 'tokens',
+        'format': 'json',
+        'formatversion': 2,
+    }
+    reply = api_request(params)
+    token = reply['query']['tokens']['csrftoken']
+
+
+def userinfo_call():
+    params = {'action': 'query', 'meta': 'userinfo', 'format': 'json'}
+    return api_request(params)
+
+
+def get_username():
+    if 'owner_key' not in session:
+        return  # not authorized
+
+    if 'username' in session:
+        return session['username']
+
+    reply = userinfo_call()
+    if 'query' not in reply:
+        return
+    session['username'] = reply['query']['userinfo']['name']
+
+    return session['username']
