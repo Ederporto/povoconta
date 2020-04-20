092ae295b7ff0ece7518aee9d1c3c4fe2fffa9c9
Ederporto <eder.porto@hotmail.com>
2020-04-20 03:19:41 -0300

    Adding error page

--- app.py
+++ app.py
@@ -131,6 +131,36 @@ def logout():
 ############################################################################
 # MUSEU DO IPIRANGA                                                        #
 ############################################################################
+@app.errorhandler(400)
+@app.errorhandler(401)
+@app.errorhandler(403)
+@app.errorhandler(404)
+@app.errorhandler(405)
+@app.errorhandler(406)
+@app.errorhandler(408)
+@app.errorhandler(409)
+@app.errorhandler(410)
+@app.errorhandler(411)
+@app.errorhandler(412)
+@app.errorhandler(413)
+@app.errorhandler(414)
+@app.errorhandler(415)
+@app.errorhandler(416)
+@app.errorhandler(417)
+@app.errorhandler(418)
+@app.errorhandler(422)
+@app.errorhandler(423)
+@app.errorhandler(424)
+@app.errorhandler(429)
+@app.errorhandler(500)
+@app.errorhandler(501)
+@app.errorhandler(502)
+@app.errorhandler(503)
+@app.errorhandler(504)
+@app.errorhandler(505)
+def page_not_found(e):
+    return render_template('error.html')
+
 @app.route('/', methods=['GET'])
 def museudoipiranga():
     username = wikidata_oauth.get_username()
